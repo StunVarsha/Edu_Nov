66e8fc16ede32199040d945d1ce1f12ee5ada5f9
StunVarsha <varshamadaan376@gmail.com>
2021-01-02 19:14:38 +0530

    a

--- var/os.js
+++ var/os.js
@@ -1,3 +1,7 @@
 var os = require('os');
 console.log(os.platform())
 console.log(os.uptime())
+console.log(os.freemem())
+console.log(os.arch());
+console.log(`${os.cpus().length} CPUs`)
+console.log(`${os.uptime()} sec`)
